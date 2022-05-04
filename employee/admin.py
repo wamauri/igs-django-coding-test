703b66b24f8f7bbaae53a56c6f8b1702d7eb79cd
Amauri Clementino <amaurisantospro@gmail.com>
2022-05-04 14:29:45 -0300

    Models and admin panel configurations

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from employee.models import Employee, Department
 
-# Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'department',
+        'created_at',
+        'active')
+    list_filter = ('created_at', 'department')
+    search_fields = ('email', 'department')
+
+
+admin.site.register(Employee, EmployeeAdmin)
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('department','created_at','active')
+    list_filter = ('created_at', 'department')
+    search_fields = ('department',)
+
+
+admin.site.register(Department, DepartmentAdmin)
--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Department(Base):
+    department = models.CharField("Department", max_length=100)
+
+    class Meta:
+        verbose_name = "Department"
+        verbose_name_plural = "Departments"
+
+    def __str__(self) -> str:
+        return self.department
+
+
+class Employee(Base):
+    name = models.CharField("Name", max_length=50)
+    email = models.CharField("E-mail", max_length=50, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
+    def __str__(self) -> str:
+        return self.name
+
