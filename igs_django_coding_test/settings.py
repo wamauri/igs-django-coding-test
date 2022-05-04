702c1158a1150923e6eaec328fbf639e51e152e3
Amauri Clementino <amaurisantospro@gmail.com>
2022-05-04 15:35:43 -0300

    Django Rest Framework configuration

--- igs_django_coding_test/settings.py
+++ igs_django_coding_test/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # Third party apps
+    # Apps
     'employee',
+
+    # Third party libs
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
